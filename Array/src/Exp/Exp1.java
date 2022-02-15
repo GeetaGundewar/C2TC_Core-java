2fd1527a8f9c172d6ef13424a146a446b5f84219
USER <USER@DESKTOP-RL67Q40>
2022-02-15 09:05:14 +0530

    first commit

---
+++ Array/src/Exp/Exp1.java
@@ -0,0 +1,24 @@
+package Exp;
+public class Exp1 
+{
+	public static void main(String[] args)
+	{
+		int array[][]= {{42,2,43},{5,60,12},{19,10,1}};
+			function(array);
+	}
+			public static void function (int array[][])
+			{
+				for(int i =0;i<3;i++)
+				{
+					for(int j=0;j<3;j++)
+					{
+						System.out.print(array[j][i]+ "   ");
+
+					}
+				
+	System.out.println();	
+				}
+			}
+
+	}
+
